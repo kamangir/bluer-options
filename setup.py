44591f7cd73f5b18c9d414e66f07d3fa48ea53ef
kamangir <arash@kamangir.net>
2025-05-09 06:44:14 +0330

    start of config-fix-2025-05-09-nkg5fx 🌀 - kamangir/bolt#746

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     include_package_data=True,
     package_data={
         NAME: [
-            "env/cconfig.env",
+            "env/config.env",
             ".bash/**/*.sh",
             "assets/*",
         ],
