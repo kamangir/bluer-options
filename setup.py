82b2dbc98897b0c29daa6db4be9579ff429e5645
kamangir <arash@kamangir.net>
2025-06-02 10:31:34 +0330

    hot-fix - kamangir/bolt#746

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     include_package_data=True,
     package_data={
         NAME: [
-            "env/config.env",
+            "config.env",
             ".bash/**/*.sh",
             "assets/*",
         ],
